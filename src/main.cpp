2b37a220b8420f5644af323b6dd2c5be1184c367
Louis Nguyen <louis.nguyen550@gmail.com>
2026-06-20 17:30:48 -0400

    add menu loop

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,36 @@ void printAllProducts(const std::vector<T> &inventory)
     }
 }
 
+void menu()
+{
+    std::cout << "=== Factory Tracker ===\n" << "1. Add product\n" << "2. List product\n" << "3. Add machine\n" << "4. List machines\n" << "5. Toggle machine on/off\n" << "0. Exit\n" << "Choice: ";
+}
+
+int getChoice()
+{
+    int input{};
+
+    std::cin >> input;
+
+    return input;
+}
+
 int main()
 {
     std::vector<Product> inventory;
+    int choice{};
 
-    inventory.push_back(Product("Steel Bolt M*", 500, 0.15));
-    inventory.push_back(Product("Aluminium Plate", 30, 12.50));
-    inventory.push_back(Product("Drive Belt", 20, 8.00));
+    do {
+        menu();
+        choice = getChoice();
 
-    printAllProducts(inventory);
+    } while (choice != 0);
+
+    // inventory.push_back(Product("Steel Bolt M*", 500, 0.15));
+    // inventory.push_back(Product("Aluminium Plate", 30, 12.50));
+    // inventory.push_back(Product("Drive Belt", 20, 8.00));
+
+    // printAllProducts(inventory);
 
     return 0;
 }
