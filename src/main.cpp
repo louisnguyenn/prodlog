8f9ac8c4c85beed2316c9641a4dca65822b7515a
Louis Nguyen <louis.nguyen550@gmail.com>
2026-06-20 17:21:36 -0400

    implement and test product and machine class

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
+#include "../include/Machine.h"
 #include "../include/Product.h"
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void printAllProducts(const std::vector<T> &inventory)
+{
+    std::cout << "=== Products (" << inventory.size() << ") ===\n";
+
+    for (const auto &product : inventory)
+    {
+        product.printInfo();
+    }
+}
 
 int main()
 {
-    Product bolt{"Steel Bolt M8", 500, 0.15};
+    std::vector<Product> inventory;
 
-    bolt.printInfo();
+    inventory.push_back(Product("Steel Bolt M*", 500, 0.15));
+    inventory.push_back(Product("Aluminium Plate", 30, 12.50));
+    inventory.push_back(Product("Drive Belt", 20, 8.00));
+
+    printAllProducts(inventory);
 
     return 0;
 }
