975f70fd27eb5814ea0213837214721175078f24
Louis Nguyen <louis.nguyen550@gmail.com>
2026-06-22 20:51:36 -0400

    implement add and read operations

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,16 @@
 #include <iostream>
 #include <vector>
 
-template <typename T>
-void printAllProducts(const std::vector<T> &inventory)
-{
-    std::cout << "=== Products (" << inventory.size() << ") ===\n";
-
-    for (const auto &product : inventory)
-    {
-        product.printInfo();
-    }
-}
-
 void menu()
 {
-    std::cout << "=== Factory Tracker ===\n" << "1. Add product\n" << "2. List product\n" << "3. Add machine\n" << "4. List machines\n" << "5. Toggle machine on/off\n" << "0. Exit\n" << "Choice: ";
+    std::cout << "=== Factory Tracker ===\n"
+              << "1. Add product\n"
+              << "2. List product\n"
+              << "3. Add machine\n"
+              << "4. List machines\n"
+              << "5. Toggle machine on/off\n"
+              << "0. Exit\n"
+              << "Choice: ";
 }
 
 int getChoice()
@@ -28,14 +24,96 @@ int getChoice()
     return input;
 }
 
+void printAllProducts(const std::vector<Product> &inventory)
+{
+    std::cout << "=== Products (" << inventory.size() << ") ===\n";
+
+    for (const auto &product : inventory)
+    {
+        product.printInfo();
+    }
+}
+
+void printAllMachines(const std::vector<Machine> &machines)
+{
+    std::cout << "=== Products (" << machines.size() << ") ===\n";
+
+    for (const auto &machine : machines)
+    {
+        machine.printInfo();
+    }
+}
+
+void addProduct(std::vector<Product> &inventory)
+{
+    std::string name{};
+    int quantity{};
+    double price{};
+
+    std::cout << "Product name: ";
+    std::cin >> name;
+
+    std::cout << "Quantity: ";
+    std::cin >> quantity;
+
+    std::cout << "Price per unit: ";
+    std::cin >> price;
+
+    inventory.push_back(Product(name, quantity, price));
+
+    std::cout << "Product added!\n";
+}
+
+void addMachine(std::vector<Machine> &machines)
+{
+    std::string name{};
+    int id{};
+    std::string type{};
+
+    std::cout << "ID: ";
+    std::cin >> id;
+
+    std::cout << "Product name: ";
+    std::cin >> name;
+
+    std::cout << "Type of machine: ";
+    std::cin >> type;
+
+    machines.push_back(Machine(id, name, type));
+
+    std::cout << "Machine added!\n";
+}
+
 int main()
 {
     std::vector<Product> inventory;
+    std::vector<Machine> machines;
     int choice{};
 
-    do {
+    do
+    {
         menu();
         choice = getChoice();
+
+        switch (choice)
+        {
+        case 1:
+            addProduct(inventory);
+            break;
+        case 2:
+            printAllProducts(inventory);
+            break;
+        case 3:
+            addMachine(machines);
+            break;
+        case 4:
+            printAllProducts(machines);
+            break;
+        case 5:
+            break;
+        case 0:
+            break;
+        }
 
     } while (choice != 0);
 
